273c7ff056858c6cc49b106f2720b09593b4b39b
Jarren Leo <jarren.leoo@gmail.com>
2023-10-07 17:56:49 +0800

    Stored algolia api key

--- data/novelshipAPI.js
+++ data/novelshipAPI.js
@@ -1,6 +1,9 @@
+import { config } from "dotenv";
+config();
+
 export const getNovelshipData = async (sku) => {
   const response = await fetch(
-    "https://n8ibe2vbxj-dsn.algolia.net/1/indexes/prod_products/query?x-algolia-agent=Algolia for JavaScript (4.10.3); Browser (lite)&x-algolia-api-key=a6d4df9f20747fe6ac25247993a4ac8c&x-algolia-application-id=N8IBE2VBXJ",
+    `https://n8ibe2vbxj-dsn.algolia.net/1/indexes/prod_products/query?x-algolia-agent=Algolia for JavaScript (4.10.3); Browser (lite)&x-algolia-api-key=${process.env.ALGOLIA_API_KEY}&x-algolia-application-id=N8IBE2VBXJ`,
     {
       method: "POST",
       body: JSON.stringify({ query: `${sku}`, hitsPerPage: "1" }),
